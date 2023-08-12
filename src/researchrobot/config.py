92d6cc261cebac0082d1062516a491d56c45d669
Eric Busboom <eric@civicknowledge.com>
2023-08-12 16:30:13 -0700

    Add /usr/local/etc/ as a configuration path

--- src/researchrobot/config.py
+++ src/researchrobot/config.py
@@ -3,29 +3,49 @@ from pathlib import Path
 from dotenv import dotenv_values
 
 
-def get_config(path: Path):
+def get_config(path: Path = None):
 
-    if not isinstance(path, Path):
+    if path is not None and not isinstance(path, Path):
         path = Path(path)
 
-    if path.is_dir():
+    if path is not None and path.is_dir():
         return {
             **dotenv_values(path / ".env"),
             **dotenv_values(path / ".env.shared"),  # load shared development variables
             **dotenv_values(path / ".env.secret"),  # load sensitive variables
         }
-    else:
+    elif path is not None:
         return dotenv_values(path)
 
+    else:
 
-config = {
-    **dotenv_values(Path("/usr/local/env/researchrobot.env")),
-    **dotenv_values(Path().home().joinpath(".researchrobot.env")),
+        all_config = []
+        for p in extant_paths():
+            all_config.extend(dotenv_values(p).items())
+
+        d = dict(all_config)
+        d["_loaded"] = [str(p) for p in extant_paths()]
+
+        return d
+
+
+# Configuration paths, in order of precedence
+conf_paths = [
+    Path("/usr/local/etc/researchrobot.env"),
+    Path().home().joinpath(".researchrobot.env"),
     # **os.environ,  # override loaded values with environment variables,
-    **dotenv_values("../.env"),
-    **dotenv_values("../.env.shared"),  # load shared development variables
-    **dotenv_values("../.env.secret"),  # load sensitive variables
-    **dotenv_values(".env"),
-    **dotenv_values(".env.shared"),  # load shared development variables
-    **dotenv_values(".env.secret"),  # load sensitive variables
-}
+    Path("../.env"),
+    Path("../.env.shared"),  # load shared development variables
+    Path("../.env.secret"),  # load sensitive variables
+    Path(".env"),
+    Path(".env.shared"),  # load shared development variables
+    Path(".env.secret"),  # load sensitive variables
+]
+
+
+def extant_paths():
+    """return the configuration paths that exist"""
+    return [p for p in conf_paths if p.exists()]
+
+
+config = get_config()
