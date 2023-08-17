7a7e6b0051ea299724ec6ec9eea2b856d3a100ba
Eric Busboom <eric@civicknowledge.com>
2023-08-17 13:32:13 -0700

    Downloading pogress bars

--- src/researchrobot/download.py
+++ src/researchrobot/download.py
@@ -5,6 +5,7 @@ from urllib.parse import urlparse
 import boto3
 import requests
 from slugify import slugify
+from tqdm.auto import tqdm
 
 
 def cache_path(url, directory):
@@ -21,7 +22,7 @@ def cache_path(url, directory):
     return filepath
 
 
-def web_download(url, directory):
+def web_download(url, directory, force=False):
 
     filepath = cache_path(url, directory)
 
@@ -29,15 +30,29 @@ def web_download(url, directory):
     if filepath.exists():
         return filepath
     else:
+        head = requests.head(url)
+        total_length = int(head.headers.get("content-length", 0))
+
         # If file doesn't exist, download and save it
         response = requests.get(url)
+
+        pb = tqdm(total=total_length, unit="B", unit_scale=True)
+
+        block_size = 64 * 1024
+
         with open(filepath, "wb") as file:
-            file.write(response.content)
+
+            for data in response.iter_content(block_size):
+                pb.update(len(data))
+                file.write(data)
+
+            c = response.content
+            file.write(c)
 
         return filepath
 
 
-def s3_download(url, directory):
+def s3_download(url, directory, force=False):
 
     filepath = cache_path(url, directory)
 
@@ -47,12 +62,20 @@ def s3_download(url, directory):
     else:
         up = urlparse(url)
         s3 = boto3.client("s3")
-        s3.download_file(up.netloc, up.path.lstrip("/"), str(filepath))
+
+        bucket, key = up.netloc, up.path.lstrip("/")
+
+        meta_data = s3.head_object(Bucket=bucket, Key=key)
+        total_length = int(meta_data.get("ContentLength", 0))
+
+        pb = tqdm(total=total_length, unit="B", unit_scale=True)
+
+        s3.download_file(bucket, key, str(filepath), Callback=pb.update)
 
         return filepath
 
 
-def cache_dl(url, directory=None):
+def cache_dl(url, directory=None, force=False):
     """Download a file to a cache directory. If the cache directory is not specified,
     use the default cache directory, specified by the environment variable
     RESEARCH_ROBOT_DEFAULT_CACHE, or 'rrcache' if not set.
@@ -64,9 +87,9 @@ def cache_dl(url, directory=None):
     up = urlparse(url)
 
     if up.scheme == "s3":
-        return s3_download(url, directory)
+        return s3_download(url, directory, force=force)
     elif up.scheme == "http" or up.scheme == "https":
-        return web_download(url, directory)
+        return web_download(url, directory, force=force)
     else:
         raise Exception(f"Unknown scheme: {up.scheme}")
 
